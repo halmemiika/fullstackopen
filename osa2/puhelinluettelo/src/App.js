bbdc38332d564a35ddf590b5db928bfafcf4b039
halmemiika <miika.halme2@gmail.com>
2021-06-16 18:49:26 +0300

    finished osa2

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
 import phoneBookService from "./services/persons";
 
+const Notification = ({ message, messageClass }) => {
+  if (message === null) {
+    return null;
+  } else if (messageClass === "success") {
+    return <div className="success">{message}</div>;
+  } else if (messageClass === "error") {
+    return <div className="error">{message}</div>;
+  }
+};
+
 const Filter = ({ handleFilter }) => {
   return (
     <>
@@ -44,6 +54,8 @@ const App = () => {
   const [newNumber, setnewNumber] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [filterRule, setFilter] = useState("");
+  const [message, setMessage] = useState(null);
+  const [messageClass, setMessageClass] = useState("success");
 
   useEffect(() => {
     phoneBookService
@@ -64,21 +76,41 @@ const App = () => {
           `${newName} is already added to phonebook, replace the old number with a new one?`
         )
       ) {
-        phoneBookService.update(person.id, nameObject).then((data) => {
-          setPersons(
-            persons.map((newPerson) =>
-              newPerson.id !== data.id ? newPerson : data
-            )
-          );
-          setNewName("");
-          setnewNumber("");
-        });
+        phoneBookService
+          .update(person.id, nameObject)
+          .then((data) => {
+            setPersons(
+              persons.map((newPerson) =>
+                newPerson.id !== data.id ? newPerson : data
+              )
+            );
+            setMessage(`Changed number of ${person.name} successfully.`);
+            setTimeout(() => {
+              setMessage(null);
+            }, 5000);
+            setNewName("");
+            setnewNumber("");
+          })
+          .catch((error) => {
+            setMessage(
+              `Information of ${person.name} has already been removed from server`
+            );
+            setMessageClass("error");
+            setTimeout(() => {
+              setMessage(null);
+              setMessageClass("success");
+            }, 5000);
+          });
       }
     } else if (person && person.number === newNumber) {
       alert(`${person.name} is already added to the phonebook!`);
     } else {
       phoneBookService.create(nameObject).then((data) => {
         setPersons(persons.concat(data));
+        setMessage(`Added ${newName}`);
+        setTimeout(() => {
+          setMessage(null);
+        }, 5000);
         setNewName("");
         setnewNumber("");
       });
@@ -98,6 +130,10 @@ const App = () => {
               .filter((person) => person.name !== undefined)
           )
         );
+      setMessage(`Deleted ${personEntry.name}`);
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
     }
   };
 
@@ -123,6 +159,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} messageClass={messageClass} />
       <Filter handleFilter={handleFilter} />
       <h3>Add a new</h3>
       <PersonForm
