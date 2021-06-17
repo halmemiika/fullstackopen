05922aedb545626ec4865c7446b7e167165ea637
halmemiika <miika.halme2@gmail.com>
2021-06-17 16:24:25 +0300

    completed until 3.11

---
+++ osa3/puhelinluettelo/README.md
@@ -0,0 +1,3 @@
+Backend in heroku: https://fierce-springs-73136.herokuapp.com/api/persons
+
+Whole app: https://fierce-springs-73136.herokuapp.com/
--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("build"));
+app.use(cors());
 
 morgan.token("json", (req, res) => {
   const method = req.method;
@@ -96,7 +99,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
