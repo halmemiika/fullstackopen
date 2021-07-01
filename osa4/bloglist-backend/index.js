fc17babe9bec047c7cc08f3c5526ab9938728a37
halmemiika <miika.halme2@gmail.com>
2021-07-01 11:12:04 +0300

    added osa4

---
+++ osa4/bloglist-backend/index.js
@@ -0,0 +1,71 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+morgan.token("json", (req) => {
+  const method = req.method;
+  if (method === "POST") {
+    return JSON.stringify(req.body);
+  }
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :json")
+);
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
