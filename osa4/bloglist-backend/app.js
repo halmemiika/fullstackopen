2f1f37e34d3153725a87061a03357899624bad83
halmemiika <miika.halme2@gmail.com>
2022-01-27 12:25:59 +0200

    added frontend to the commit

--- osa4/bloglist-backend/app.js
+++ osa4/bloglist-backend/app.js
@@ -34,6 +34,7 @@ app.use(
   )
 );
 app.use(middleware.tokenExtractor);
+app.use(express.static("build"));
 
 app.use("/api/blogs", middleware.userExtractor, blogsRouter);
 app.use("/api/users", userRouter);
